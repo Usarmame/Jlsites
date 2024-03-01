405b4626b1d37921abbc94817325f806402c1739
Usarmame <edwinmachuza02@gmail.com>
2024-03-01 16:23:37 +0200

    ol

--- Jlsites/Javascript/script.js
+++ Jlsites/Javascript/script.js
@@ -2,8 +2,13 @@ let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 
 trilho.addEventListener('click', ()=>{
-
     trilho.classList.toggle('dark')
     body.classList.toggle('dark')
+    function In(){
+        var hover = window.document.getElementById(hover)
+        hover.style.backgroundColor ='#313131de'
+    }
 })
+
+    
 
